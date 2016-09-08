8111dfcb3d2571911375541c4a7f204eaf80b36d
kickscar <kickscar@gmail.com>
2016-09-08 20:59:53 +0900

    @AuthUser & HandlerMethodArgumentResolver 구현

--- src/main/java/kr/co/saramin/mysite/controller/UserController.java
+++ src/main/java/kr/co/saramin/mysite/controller/UserController.java
@@ -4,12 +4,15 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import kr.co.saramin.mysite.service.UserService;
 import kr.co.saramin.mysite.vo.UserVo;
+import kr.co.saramin.security.annotation.Auth;
+import kr.co.saramin.security.annotation.AuthUser;
 
 @Controller
 @RequestMapping( "/user" )
@@ -17,6 +20,18 @@ public class UserController {
 	@Autowired
 	UserService userService;
 
+	@Auth
+	@RequestMapping( "/updateform" )
+	public String updateform( @AuthUser UserVo authUser, Model model ) {
+		
+		System.out.println( authUser );
+//		UserVo userVo = userService.getUser( authUser.getNo() );
+//		model.addAttribute( "userVo", userVo );
+		
+		return "user/updateform";
+	}
+	
+	@Auth
 	@RequestMapping( "/update" )
 	public String update( HttpSession session, @ModelAttribute UserVo userVo ) {
 		UserVo authUser = (UserVo)session.getAttribute( "authUser" );
@@ -45,15 +60,6 @@ public class UserController {
 	public String loginform(){
 		return "user/loginform";
 	}
-	
-	@RequestMapping( "/logout" )
-	public String logout( HttpSession session ){
-		session.removeAttribute( "authUser" );
-		session.invalidate();
-		
-		return "redirect:/";
-	}
-	
 
 	
 }
