2148d2e9d1552ae9b472eba4fcc85937ee753043
kickscar <kickscar@gmail.com>
2016-09-08 15:35:00 +0900

    collision solved

--- src/main/java/kr/co/saramin/mysite/dao/GuestbookDao.java
+++ src/main/java/kr/co/saramin/mysite/dao/GuestbookDao.java
@@ -1,6 +1,7 @@
 package kr.co.saramin.mysite.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
